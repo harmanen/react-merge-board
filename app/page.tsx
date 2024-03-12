8f7a777d0719225b1029506e3eab5468cc722b4f
Harmanen <hurmanen@gmail.com>
2024-03-12 15:11:30 +0200

    Create initial layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import Image from 'next/image';
+import Box from '@mui/material/Box';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div>APP</div>
+      <Box className={styles.contentContainer}>
+        <Box className={styles.infoContainer}>INFO</Box>
+        <Box className={styles.gridContainer}>GRID</Box>
+      </Box>
     </main>
   );
 }
