6b4a596f23ca124bdd74d60c3fca17d8f1e401d4
Harmanen <hurmanen@gmail.com>
2024-03-13 20:56:11 +0200

    Add a disclaimer text

--- app/components/InfoBox.tsx
+++ app/components/InfoBox.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import styles from '../page.module.css';
+import { version } from '../../package.json';
 
 export function InfoBox() {
-  return <Box className={styles.infoContainer}>INFO</Box>;
+  return (
+    <Box className={styles.infoContainer}>
+      `THIS IS VERSION {version} AND VERY MUCH WORK IN PROGRESS! Items can be
+      moved around, though`
+    </Box>
+  );
 }
