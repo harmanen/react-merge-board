b3c833e8e16cc03e829ee67fb0d6685b7885d72e
Harmanen <hurmanen@gmail.com>
2024-03-13 21:02:47 +0200

    Prefer default import

--- app/components/InfoBox.tsx
+++ app/components/InfoBox.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import styles from '../page.module.css';
-import { version } from '../../package.json';
+import packageJSON from '../../package.json';
 
 export function InfoBox() {
   return (
     <Box className={styles.infoContainer}>
-      `THIS IS VERSION {version} AND VERY MUCH WORK IN PROGRESS! Items can be
-      moved around, though`
+      `THIS IS VERSION {packageJSON.version} AND VERY MUCH WORK IN PROGRESS!
+      Items can be moved around, though`
     </Box>
   );
 }
