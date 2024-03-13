ff8aaad5ef31d567ae624471365e67c42e060b56
Harmanen <hurmanen@gmail.com>
2024-03-13 21:29:34 +0200

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
-  basePath: '/react-merge-board',
+  assetPrefix: process.env.ASSET_PREFIX,
+  basePath: process.env.BASE_PATH,
 };
 
 export default nextConfig;
