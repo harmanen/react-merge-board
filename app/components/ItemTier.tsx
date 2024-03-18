b20aa36fb02bd2715c66ffa5149f01a4eef44f83
Harmanen <hurmanen@gmail.com>
2024-03-18 18:01:18 +0200

    Add "bubble" for item tier number

--- app/components/ItemTier.tsx
+++ app/components/ItemTier.tsx
@@ -6,11 +6,30 @@ interface Props {
 }
 
 export function ItemTier({ itemLevel }: Props) {
+  // Use black font color for certain cases to get some contrast
+  let textColor;
+
+  switch (itemLevel) {
+    case 1:
+    case 11:
+      textColor = '#000000';
+      break;
+
+    default:
+      textColor = tierColorMap[itemLevel];
+      break;
+  }
+
   return (
-    <Box className="item-tier-container">
+    <Box
+      className="item-tier-container"
+      style={{
+        background: `radial-gradient(circle, #FFFFFF, #FFFFFF 40%, ${tierColorMap[itemLevel]})`,
+      }}
+    >
       <Typography
         className="item-tier-text"
-        style={{ color: tierColorMap[itemLevel] }}
+        style={{ color: textColor }}
       >
         {itemLevel}
       </Typography>
