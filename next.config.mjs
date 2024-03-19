0ab6a16db437e0f1b7d783f62347afca1d189464
Harmanen <hurmanen@gmail.com>
2024-03-19 08:41:08 +0200

    Edit Next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+// Recognize production env and use repo slug as path
+// -> static files are now properly found on prod AND local etc.
+const isProduction = process.env.GITHUB_ACTIONS || false;
+const path = isProduction ? '/react-merge-board' : '';
+
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
-  basePath: '/react-merge-board',
-  assetPrefix: '/react-merge-board',
+  basePath: path,
+  assetPrefix: path,
 };
 
 export default nextConfig;
