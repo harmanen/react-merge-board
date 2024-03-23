245a7476fc748a03be7538c96f400dbeca18929e
Harmanen <hurmanen@gmail.com>
2024-03-23 13:54:56 +0200

    Display default text if no cell is selected

--- app/components/InfoBox.tsx
+++ app/components/InfoBox.tsx
@@ -4,6 +4,7 @@ import packageJSON from '../../package.json';
 import { ItemOnBoard } from './Board.type';
 import { UniqueIdentifier } from '@dnd-kit/core';
 import './InfoBox.css';
+import ScaledTypography from './ScaledTypography';
 
 interface InfoBox {
   activeCellIndex: UniqueIdentifier | undefined;
@@ -13,18 +14,33 @@ interface InfoBox {
 export function InfoBox({ activeCellIndex, activeItem }: InfoBox) {
   return (
     <Box className="info-container">
+      {/* Title */}
       <Typography
         align="center"
         variant="h6"
       >
         Merge board
       </Typography>
-      <Typography align="center">Active cell: {activeCellIndex}</Typography>
-      <Typography>Data: {JSON.stringify(activeItem)}</Typography>
-      <Typography
+
+      {/* Main content */}
+      {activeCellIndex === undefined ? (
+        <Box className="no-active-cell-container">
+          <ScaledTypography align="center">
+            Start by selecting a cell!
+          </ScaledTypography>
+        </Box>
+      ) : (
+        <>
+          <Typography align="center">Active cell: {activeCellIndex}</Typography>
+          <Typography>Data: {JSON.stringify(activeItem)}</Typography>
+        </>
+      )}
+
+      {/* Version */}
+      <ScaledTypography
         className="app-version"
         variant="subtitle2"
-      >{`(v${packageJSON.version})`}</Typography>
+      >{`(v${packageJSON.version})`}</ScaledTypography>
     </Box>
   );
 }
