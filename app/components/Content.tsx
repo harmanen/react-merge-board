3ef53fe85d1174f8038a5a65984a6e6003081cba
Harmanen <hurmanen@gmail.com>
2024-03-23 14:22:22 +0200

    Detect and handle clicks outside the app area

--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -1,12 +1,15 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { InfoBox } from './InfoBox';
 import { Board } from './Board';
 import { Item } from './Board.type';
 import { UniqueIdentifier } from '@dnd-kit/core';
+import { Box } from '@mui/material';
+import styles from '../page.module.css';
 
+// Component
 interface mockData {
   width: number;
   height: number;
@@ -35,8 +38,36 @@ export default function Content({ gridIdList, mockData }: Content) {
     UniqueIdentifier | undefined
   >(undefined);
 
+  // For detecting clicks outside the app area
+  const contentWrapperRef = useRef<HTMLInputElement>(null);
+
+  // Handle cliks outside the app area -> set active cell to undefined
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent): void => {
+      if (
+        contentWrapperRef.current &&
+        !contentWrapperRef.current.contains(event.target as Node)
+      ) {
+        setActiveCellIndex(undefined);
+      }
+    };
+    // Bind the event listener
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [contentWrapperRef]);
+
   return (
-    <>
+    <Box
+      className={styles.contentContainer}
+      // Dynamically define grid width
+      sx={{
+        width: `calc(6 * var(--layout-padding) + ${width} * var(--grid-size))`,
+      }}
+      ref={contentWrapperRef}
+    >
       <InfoBox
         activeCellIndex={activeCellIndex}
         activeItem={
@@ -56,6 +87,6 @@ export default function Content({ gridIdList, mockData }: Content) {
           setActiveCellIndex={setActiveCellIndex}
         />
       )}
-    </>
+    </Box>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from 'next';
-import Box from '@mui/material/Box';
 import styles from './page.module.css';
 import Content from './components/Content';
 
@@ -30,18 +29,10 @@ for (let i = 0; i < gridSize; i++) {
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Box
-        className={styles.contentContainer}
-        // Dynamically define grid width
-        sx={{
-          width: `calc(6 * var(--layout-padding) + ${width} * var(--grid-size))`,
-        }}
-      >
-        <Content
-          gridIdList={gridIdList}
-          mockData={mockData}
-        />
-      </Box>
+      <Content
+        gridIdList={gridIdList}
+        mockData={mockData}
+      />
     </main>
   );
 }
