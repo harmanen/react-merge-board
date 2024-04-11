5f3100e2b8a172b8da497de8a4e0eb3421f4f1c1
Harmanen <hurmanen@gmail.com>
2024-04-11 21:09:09 +0300

    Update test coverage for DroppableGridItem

--- __tests__/DroppableGridItem.test.tsx
+++ __tests__/DroppableGridItem.test.tsx
@@ -3,15 +3,45 @@ import { render, screen } from '@testing-library/react';
 import { DroppableGridItem } from '@/app/components/DroppableGridItem';
 
 describe('DroppableGridItem', () => {
-  const props = {
-    id: 1,
-    activeCellIndex: 1,
-    activeChainId: '',
-    chainId: '',
-  };
+  it('renders droppable grid item without chain info', () => {
+    render(
+      <DroppableGridItem
+        id={1}
+        activeCellIndex={1}
+        activeChainId={undefined}
+        chainId={undefined}
+      >
+        Test
+      </DroppableGridItem>,
+    );
+    expect(screen.getByTestId('droppable-grid-item')).toBeInTheDocument();
+  });
 
-  it('renders droppable grid item', () => {
-    render(<DroppableGridItem {...props}>Test</DroppableGridItem>);
+  it('renders droppable grid item with chain info (active cell)', () => {
+    render(
+      <DroppableGridItem
+        id={1}
+        activeCellIndex={1}
+        activeChainId={'test'}
+        chainId={'test'}
+      >
+        Test
+      </DroppableGridItem>,
+    );
+    expect(screen.getByTestId('droppable-grid-item')).toBeInTheDocument();
+  });
+
+  it('renders droppable grid item with chain info (inactive cell)', () => {
+    render(
+      <DroppableGridItem
+        id={1}
+        activeCellIndex={2}
+        activeChainId={'test'}
+        chainId={'test'}
+      >
+        Test
+      </DroppableGridItem>,
+    );
     expect(screen.getByTestId('droppable-grid-item')).toBeInTheDocument();
   });
 });
