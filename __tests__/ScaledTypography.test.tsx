f44369ca2250b1f0b558fa4bda04d133cbce6c85
Harmanen <hurmanen@gmail.com>
2024-04-11 14:57:22 +0300

    Add a test for ScaledTypography

---
+++ __tests__/ScaledTypography.test.tsx
@@ -0,0 +1,10 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import ScaledTypography from '@/app/components/ScaledTypography';
+
+describe('ScaledTypography', () => {
+  it('renders text', () => {
+    render(<ScaledTypography>Test</ScaledTypography>);
+    expect(screen.getByText('Test')).toBeInTheDocument();
+  });
+});
