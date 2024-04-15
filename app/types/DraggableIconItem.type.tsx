d57ff91a2e821d6144dc59190e5b30f2dc7e7870
Harmanen <hurmanen@gmail.com>
2024-04-15 18:08:08 +0300

    Document prop types for the DnD components

--- app/types/DraggableIconItem.type.tsx
+++ app/types/DraggableIconItem.type.tsx
@@ -1,6 +1,14 @@
 import GenericProps from './GenericProps.type';
 import { Item } from './mockData.type';
 
+/**
+ * Prop types for the DraggableIconItem component.
+ * @property iconItem - Object containing item data.
+ * @property index - Array index of the cell where the item is.
+ * @property isHidden - Is item hidden.
+ * @property isInBubble - Is item in a bubble.
+ * @property isActive - Is this the active item.
+ */
 export interface DraggableIconItem extends GenericProps {
   iconItem: Item;
   index: number;
