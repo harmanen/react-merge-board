0f5e665da3ad5fcc9b5742688ffef570891dbea7
Harmanen <hurmanen@gmail.com>
2024-04-15 15:45:11 +0300

    Document prop types for the Content component

--- app/types/Content.type.tsx
+++ app/types/Content.type.tsx
@@ -1,5 +1,11 @@
 import { mockData } from './mockData.type';
 
+/**
+ * Prop types for the Content component.
+ * @property gridIdList - List of IDs automatically generated based on grid size.
+ * Effectively, [ 0, 1, 2, ... , n ], where n = width * height - 1.
+ * @property mockData - Data object. In case of this app, example data is used.
+ */
 export interface Content {
   gridIdList: Array<number>;
   mockData: mockData;
