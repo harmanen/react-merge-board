9a9ebcc601c8efcce94f8e808742eb1e67fd1c3c
Harmanen <hurmanen@gmail.com>
2024-04-15 18:56:39 +0300

    Document prop types for the ScaledTypography component

--- app/types/ScaledTypography.type.tsx
+++ app/types/ScaledTypography.type.tsx
@@ -1,7 +1,11 @@
 import { TypographyStyle } from '@mui/material';
 
+/**
+ * Prop types for the ScaledTypography component.
+ *
+ * Needed to be manually defined to get rid of an error...
+ * TypographyStyle out of date?
+ */
 export interface ScaledTypography extends TypographyStyle {
-  // Needed to be manually defined to get rid of an error...
-  // TypographyStyle out of date?
   translate?: 'yes' | 'no' | undefined;
 }
